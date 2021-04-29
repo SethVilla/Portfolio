753d3eb63a303a5862742d9022a1b57c87e16bef
Seth Villa <sethvilla13@gmail.com>
2021-04-28 22:52:16 -0400

    Configured repo with Lint and Prettier preferences

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import logo from './logo.svg';
 import './App.css';
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
+/** @format */
+
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
