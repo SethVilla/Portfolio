ac15543b83ed72c4bbb12b9f2b489b2bf7ea8f53
Seth Villa <sethvilla13@gmail.com>
2021-05-08 12:36:51 -0400

    server lint husky

--- server/index.js
+++ server/index.js
@@ -7,3 +7,7 @@ const app = express();
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
