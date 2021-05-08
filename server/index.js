793aa8a235a797a5ceea14fb2c2ef829f806c8e9
Seth Villa <sethvilla13@gmail.com>
2021-05-08 14:49:15 -0400

    linting and  husky setup client and server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/api', (req, res) => {
+  res.json({message: 'Hello from server!'});
 });
 
 app.listen(PORT, () => {
